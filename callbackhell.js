eec9c182fb7610b9424433912459d9e5566f9049
sanjayreddyyanala <159144145+sanjayreddyyanala@users.noreply.github.com>
2025-02-22 10:23:35 +0530

    Update callbackhell.js

--- callbackhell.js
+++ callbackhell.js
@@ -1,6 +1,6 @@
 const fetchUser = (id,callback)=>{
     console.log("Fetching user name");
-    callback("varun");
+    callback("sanjay");
 }
 const fetchOrder = (username,callback) =>{
     console.log(`Fetching ${username} order`);
